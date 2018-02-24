9f93c5b6a0cd22895990d1ae7e360b5ba4c74fef
Muhammad Arshaq <arshaq71@yahoo.com>
2018-02-24 12:43:49 +0500

    My practise to upload a files on Git

---
+++ app.js
@@ -0,0 +1,56 @@
+/*=========={ASSIGNMENT JS_2 (IF & ELSE CONDITIONAL STATEMENT)}========*/
+
+
+/*======= Write a program to check whether the given input number is divisible by 3 or else show a message “Number is not divisible by 3”. =======*/
+// var a=+prompt("Enter the Integer to check the Integer is divisible by 3 ");
+// var b=a%3;
+// if(b===0){
+//     document.write("Given Intger is divisible by 3");
+// }
+// else {
+//     document.write("Given Integer is Not divisible by 3");
+// }
+
+
+
+/*======== Write a program that checks whether the given input is an even number or an odd number. ===========*/
+// var a=+prompt("Enter the Integer to check its Even | Odd ");
+// var b =a%2;
+// if(b===0){
+//     document.write("Enter Integer Is Even ="+a);
+// }
+// else{
+//     document.write("Enter Integer Is Odd ="+a);
+// }
+
+
+/*====== Write an if/else statement with the following condition: If the variable age is greater than 18, output "Old enough", otherwise output "Too young". ===========*/
+// var a=+prompt("Enter Your age");
+// if(a>=18){
+//     document.write("Old Enough = "+a);
+// }
+// else{document.write("Too Young = "+a);}
+
+
+/*============ Write a program that prompts the user for their name, and then displays a special greeting to that person if their name is the same as yours. If the name entered by the user is anything other than your name, your code should not produce any output. ===========*/
+// var a=prompt("Enter Your Name:");
+// var b=prompt("Enter the anothee name");
+// if(b==a){
+//     document.write("You enter the same Name Wellcome "+b);
+// }
+
+/*======= Write a program that adds an else statement to the following script to display “You are not Fahad” ==========*/
+// var a=prompt("Enter Username");
+// if(a==="ammad"){
+//     document.write("Welcome "+a);
+// }
+// else{document.write("Username Invalid "+a);}
+
+
+/*========= Write a JavaScript program that accept two integers and display the larger. Also show if the two integers are equal. ===========*/
+// var a = +prompt("Enter 1st Integer ");
+// var b = +prompt("Enter 2nd integer");
+// var c=a+1;
+// var d=b+1;
+// if(c==d || d==c){document.write("Your Integer are Equal 1st integer is "+c+" 2nd integer is "+d);}
+// else {document.write("Given Two integer are not equal ");}
